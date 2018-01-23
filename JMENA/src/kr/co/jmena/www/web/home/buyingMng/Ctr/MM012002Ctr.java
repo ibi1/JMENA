eca641073c9ddcdb3bc22bdac4659972cbf30aec
ejyim <ejyim@ejyim-LT>
2018-01-23 09:33:03 +0900

    *

--- JMENA/src/kr/co/jmena/www/web/home/buyingMng/Ctr/MM012002Ctr.java
+++ JMENA/src/kr/co/jmena/www/web/home/buyingMng/Ctr/MM012002Ctr.java
@@ -165,10 +165,8 @@ public class MM012002Ctr {
 			}
 			
 			ServletContext context = request.getServletContext();
-			String filePath = context.getRealPath("/") + "WEB-INF\\ExcelDownLoad\\";
+			String filePath = context.getRealPath("/") +"WEB-INF"+ File.separator +"ExcelDownLoad"+ File.separator;
 			String fileName = "MM012002_exportToExcel";
-			
-			System.out.println(filePath);
 			
 			// 엑셀 파일 생성
 			f = ex.MakeExcelFile(wb, filePath, fileName);
